e4821afa67401c258145752316c03e628dab92dd
prosper simi <logicprime001@gmail.com>
2021-07-02 13:51:00 +0100

    commented user interface logics in js for peer code reviews

--- js/scripts.js
+++ js/scripts.js
@@ -21,23 +21,25 @@ function modulo(number1, number2) {
   return number1 % number2;
 }
 
+// user interface logics:
+
 $(document).ready(function() {
   $("form#calculator").submit(function(event) {
     event.preventDefault();
-    const number1 = parseInt($("#input1").val());
-    const number2 = parseInt($("#input2").val());
+    const input1 = parseInt($("#input1").val());
+    const input2 = parseInt($("#input2").val());
     const operator = $("input:radio[name=operator]:checked").val();
-    let result;
+    let result = ""
     if (operator === "add") {
-      result = add(number1, number2);
+      result = add(input1, input2);
     } else if (operator === "subtract") {
-      result = subtract(number1, number2);
+      result = subtract(input1, input2);
     } else if (operator === "multiply") {
-      result = multiply(number1, number2);
+      result = multiply(input1, input2);
     } else if (operator === "divide") {
-      result = divide(number1, number2);
+      result = divide(input1, input2);
     } else if (operator === "modulo") {
-      result = modulo(number1, number2);
+      result = modulo(input1, input2);
     }
     $("#output").text(result);
   });
