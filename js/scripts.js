16f6eb8714a7f00bee7f0fcd90a985c73754552d
prosper simi <logicprime001@gmail.com>
2021-07-02 12:48:31 +0100

    this ismy first commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+//business logic:
+/*
+*/
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function modulo(number1, number2) {
+  return number1 % number2;
+}
+
